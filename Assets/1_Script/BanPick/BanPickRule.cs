ab34fb21bfebf7f0e2a7c79725c114d5a78b4202
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-08-05 21:16:19 +0900

    진행관리 스크립트 추가

---
+++ Assets/1_Script/BanPick/BanPickRule.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+public class BanPickRule // 진행 관리
+{
+    public void BanPickLoop(IReadOnlyList<TurnInfo> turns)
+    {
+        // 밴
+        // 픽
+        // 스왑
+    }
+}
