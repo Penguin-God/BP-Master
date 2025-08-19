ec4a3491b52940eafb18d38d7e53ff63f9b24b30
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-08-19 14:09:01 +0900

    공방 스탯 보너스 추가

--- Assets/1_Script/Domain/BanPick/StatScoreCalculator.cs
+++ Assets/1_Script/Domain/BanPick/StatScoreCalculator.cs
@@ -2,18 +2,22 @@ using System.Collections.Generic;
 
 public class StatScoreCalculator
 {
+    readonly SortedDictionary<int, int> attackBonusData;
+    readonly SortedDictionary<int, int> defenseBonusData;
     readonly SortedDictionary<int, int> rangeBonusData;
     readonly SortedDictionary<int, int> speedBonusData;
 
-    public StatScoreCalculator(SortedDictionary<int, int> rangeBonusData, SortedDictionary<int, int> speedBonusData)
+    public StatScoreCalculator(SortedDictionary<int, int> attackBonusData, SortedDictionary<int, int> defenseBonusData, SortedDictionary<int, int> rangeBonusData, SortedDictionary<int, int> speedBonusData)
     {
+        this.attackBonusData = attackBonusData;
+        this.defenseBonusData = defenseBonusData;
         this.rangeBonusData = rangeBonusData;
         this.speedBonusData = speedBonusData;
     }
 
     public int CalculateScore(int attack, int defense, int ragne, int speed)
     {
-        return attack + defense + GetBonus(rangeBonusData, ragne) + GetBonus(speedBonusData, speed);
+        return attack + defense + GetBonus(attackBonusData, attack) + GetBonus(defenseBonusData, defense) + GetBonus(rangeBonusData, ragne) + GetBonus(speedBonusData, speed);
     }
 
     int GetBonus(SortedDictionary<int, int> bands, int sum)
--- Assets/2_Tests/CalculateScoreTests.cs
+++ Assets/2_Tests/CalculateScoreTests.cs
@@ -37,25 +37,32 @@ public class CalculateScoreTests
     {
         rangeData ??= new SortedDictionary<int, int>();
         speedData ??= new SortedDictionary<int, int>();
-        var statCal = new StatScoreCalculator(rangeData, speedData);
+        var statCal = new StatScoreCalculator(new SortedDictionary<int, int>(), new SortedDictionary<int, int>(), rangeData, speedData);
         return new TeamScoreCalculator(statCal);
     }
 
     [Test]
-    [TestCase(0, 0, 0)]
-    [TestCase(17, 25, 150)]
-    [TestCase(12, 500, 100)]
-    [TestCase(21, 21, 160)]
-    public void 스탯_구간별_보너스(int range, int speed, int expected)
+    [TestCase(0, 0, 0, 0, 0)]
+    [TestCase(300, 300, 17, 25, 850)]
+    [TestCase(1000, 1000, 12, 500, 2300)]
+    [TestCase(0, 0, 21, 21, 160)]
+    public void 스탯_구간별_보너스(int attack, int defense, int range, int speed, int expected)
     {
+        SortedDictionary<int, int> bonusData1 = new SortedDictionary<int, int>()
+        {
+            { 300, 50 },
+            { 400, 80 },
+            { 500, 100 }
+        };
+
         SortedDictionary<int, int> bonusData = new SortedDictionary<int, int>()
         {
             { 15, 50 },
             { 20, 80 },
             { 25, 100 }
         };
-        StatScoreCalculator sut = new StatScoreCalculator(bonusData, bonusData);
-        int result = sut.CalculateScore(0, 0, range, speed);
+        StatScoreCalculator sut = new StatScoreCalculator(bonusData1, bonusData1, bonusData, bonusData);
+        int result = sut.CalculateScore(attack, defense, range, speed);
 
         Assert.AreEqual(expected, result);
     }
@@ -63,10 +70,12 @@ public class CalculateScoreTests
     [Test]
     public void 점수는_공방과_보너스의_합()
     {
-        StatScoreCalculator sut = new(new SortedDictionary<int, int>(), new SortedDictionary<int, int>());
+        var bonusData = new SortedDictionary<int, int>();
+        bonusData.Add(15, 50);
+        StatScoreCalculator sut = new(bonusData, bonusData, bonusData, bonusData);
         
-        int result = sut.CalculateScore(80, 30, 0, 0);
+        int result = sut.CalculateScore(80, 30, 15, 15);
 
-        Assert.AreEqual(110, result);
+        Assert.AreEqual(310, result);
     }
 }
