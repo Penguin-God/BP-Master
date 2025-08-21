3e64acd8a75b486d745952bbf99347bbc510c439
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-08-21 12:01:42 +0900

    매치 진행 테스트 제작

--- Assets/2_Tests/MatchTests.cs
+++ Assets/2_Tests/MatchTests.cs
@@ -6,8 +6,34 @@ using UnityEngine.TestTools;
 public class MatchTests
 {
     [Test]
-    public void 매치는_알아서_이루어짐()
+    public void 매치는_순서대로_진행()
     {
-        
+        MatchManager sut = new();
+
+        sut.GameStart();
+
+        Assert.AreEqual(Team.Blue, sut.CurrentTurn);
+        Assert.AreEqual(GamePhase.Ban, sut.CurrentPhase);
+
+        blue.Ban();
+        Assert.AreEqual(Team.Red, sut.CurrentTurn);
+
+        red.Ban();
+        Assert.AreEqual(Team.Blue, sut.CurrentTurn);
+        Assert.AreEqual(GamePhase.Pick, sut.CurrentPhase);
+
+        blue.Pick();
+        red.Pick();
+
+        Assert.AreEqual(Team.All, sut.CurrentTurn);
+        Assert.AreEqual(GamePhase.Swap, sut.CurrentPhase);
+
+        blue.SwapDone();
+        Assert.AreEqual(GamePhase.Swap, sut.CurrentPhase);
+
+        red.SwapDone();
+        Assert.AreEqual(GamePhase.Done, sut.CurrentPhase);
     }
+
+    // 밴픽은 알아서 저장됨
 }
