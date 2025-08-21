6d717195e3857328b8f35c0d81f79ca33ee2f0ad
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-08-21 13:18:30 +0900

    죽은코드 제거

--- Assets/2_Tests/AgentActionTests.cs
+++ Assets/2_Tests/AgentActionTests.cs
@@ -1,7 +1,5 @@
 using NUnit.Framework;
-using System;
 using System.Collections;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -67,55 +65,6 @@ public class AgentActionTests
         sut.SwapDone(Team.Red);
         Assert.IsFalse(isDone);
         sut.SwapDone(Team.Blue);
-        Assert.IsTrue(isDone);
-    }
-
-    [Test]
-    public void 에이전트_밴_알림()
-    {
-        bool isDone = false;
-        TeamBanPickStorage storage = new();
-        var agent = new ActionAgent(storage);
-        agent.OnActionDone += () => isDone = true;
-
-        agent.Ban(1);
-
-        Assert.AreEqual(1, storage.GetStorage(SelectType.Ban).Count);
-        Assert.AreEqual(1, storage.GetStorage(SelectType.Ban)[0]);
-        Assert.IsTrue(isDone);
-    }
-
-    [Test]
-    public void 에이전트_픽_알림()
-    {
-        bool isDone = false;
-        TeamBanPickStorage storage = new();
-        var agent = new ActionAgent(storage);
-        agent.OnActionDone += () => isDone = true;
-
-        agent.Pick(1);
-
-        Assert.AreEqual(1, storage.GetStorage(SelectType.Pick).Count);
-        Assert.AreEqual(1, storage.GetStorage(SelectType.Pick)[0]);
-        Assert.IsTrue(isDone);
-    }
-
-    [Test]
-    public void 에이전트_스왑_알림()
-    {
-        bool isDone = false;
-        TeamBanPickStorage storage = new();
-        storage.SaveSelect(SelectType.Pick, 1);
-        storage.SaveSelect(SelectType.Pick, 2);
-        var agent = new ActionAgent(storage);
-        agent.OnActionDone += () => isDone = true;
-
-        agent.Swap(0, 1);
-        Assert.IsFalse(isDone);
-        Assert.AreEqual(2, storage.GetStorage(SelectType.Pick)[0]);
-        Assert.AreEqual(1, storage.GetStorage(SelectType.Pick)[1]);
-
-        agent.SwapDone();
         Assert.IsTrue(isDone);
     }
 }
