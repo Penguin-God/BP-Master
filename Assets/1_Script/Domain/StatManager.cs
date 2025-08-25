b62c6b9dacf659c4c73fa1c9a6b29898a82d3913
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-08-25 17:13:13 +0900

    statmanager 테스트 통과

---
+++ Assets/1_Script/Domain/StatManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Side { Self, Opponent, All }
+public class StatManager
+{
+    ChampionStatData[] self;
+    ChampionStatData[] opponent;
+    public IReadOnlyList<ChampionStatData> Self => self;
+    public IReadOnlyList<ChampionStatData> Opponent => opponent;
+
+    public StatManager(IEnumerable<ChampionStatData> self, IEnumerable<ChampionStatData> opponent)
+    {
+        this.self = self.ToArray();
+        this.opponent = opponent.ToArray();
+    }
+
+
+    public IReadOnlyList<ChampionStatData> GetData(Side side) => side == Side.Self ? self : opponent;
+
+    public void Update(Side side, int index, Func<ChampionStatData, ChampionStatData> mutator)
+    {
+        var arr = side == Side.Self ? self : opponent;
+        arr[index] = mutator(arr[index]);
+    }
+
+    public void UpdateAll(Side side, Func<ChampionStatData, ChampionStatData> mutator)
+    {
+        if (side == Side.Self) self = self.Select(mutator).ToArray();
+        else if(side == Side.Opponent) opponent = opponent.Select(mutator).ToArray();
+    }
+}
---
+++ Assets/2_Tests/StatManagementTests.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class StatManagementTests
+{
+    ChampionStatData CreateData(int atk, int def = 0, int range = 0, int speed = 0) => new ChampionStatData(atk, def, range, speed);
+
+    [Test]
+    public void 지정한_측과_인덱스만_변경()
+    {
+        // arrange
+        var sut = new StatManager(
+            self: new[] { CreateData(10), CreateData(20) },
+            opponent: new[] { CreateData(40), CreateData(50) }
+        );
+
+        // act: 상대 측(index 1)의 공격력만 -12
+        sut.Update(Side.Opponent, 1, c => new ChampionStatData(c.Attack - 12, c.Defense, c.Range, c.Speed));
+
+        // assert: 타겟만 변경, 나머지는 그대로
+        Assert.AreEqual(40, sut.Opponent[0].Attack);
+        Assert.AreEqual(38, sut.Opponent[1].Attack);
+        Assert.AreEqual(10, sut.Self[0].Attack);
+        Assert.AreEqual(20, sut.Self[1].Attack);
+    }
+
+    [Test]
+    public void 결과가_음수면_0()
+    {
+        // arrange
+        var sut = new StatManager(
+            self: new[] { CreateData(5) },
+            opponent: Array.Empty<ChampionStatData>()
+        );
+
+        // act: -999 시도 → ChampionStatData 생성자에서 0으로 클램프
+        sut.Update(Side.Self, 0, c => new ChampionStatData(c.Attack - 999, c.Defense, c.Range, c.Speed));
+
+        // assert
+        Assert.AreEqual(0, sut.Self[0].Attack);
+    }
+
+    [Test]
+    public void 지정한_측의_모든_원소_변경()
+    {
+        // arrange
+        var sut = new StatManager(
+            self: new[] { CreateData(10), CreateData(20) },
+            opponent: new[] { CreateData(30), CreateData(40) }
+        );
+
+        // act: Self 전원 공격력 -12 (음수는 구조체 생성자에서 0 클램프)
+        sut.UpdateAll(Side.Self, c => new ChampionStatData(c.Attack - 12, c.Defense, c.Range, c.Speed));
+
+        // assert
+        Assert.AreEqual(0, sut.Self[0].Attack); // 10 -> 0
+        Assert.AreEqual(8, sut.Self[1].Attack); // 20 -> 8
+        Assert.AreEqual(30, sut.Opponent[0].Attack);
+        Assert.AreEqual(40, sut.Opponent[1].Attack);
+    }
+}
