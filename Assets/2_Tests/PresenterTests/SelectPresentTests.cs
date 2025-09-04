244495469591ea0a9e68bd26376e40ffecf41e92
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-04 15:19:28 +0900

    프레젠터 선택 대행 테스트 작성

--- Assets/2_Tests/PresenterTests/SelectPresentTests.cs
+++ Assets/2_Tests/PresenterTests/SelectPresentTests.cs
@@ -5,6 +5,18 @@ using UnityEngine.TestTools;
 
 public class SelectPresentTests
 {
-    //[Test]
-    //public void 
+    [Test]
+    public void 프레젠터_선택_대행()
+    {
+        GameBanPickStorage storage = new(new int[] { 1, 2, 3 });
+        SelectPresenter sut = new();
+
+        Assert.IsTrue(sut.SelectChampion(GamePhase.Pick, Team.Blue, 1));
+        Assert.AreEqual(1, storage.GetStorage(Team.Blue, SelectType.Pick));
+
+        Assert.IsTrue(sut.SelectChampion(GamePhase.Ban, Team.Red, 2));
+        Assert.AreEqual(1, storage.GetStorage(Team.Red, SelectType.Ban));
+
+        Assert.IsFalse(sut.SelectChampion(GamePhase.Swap, 3));
+    }
 }
