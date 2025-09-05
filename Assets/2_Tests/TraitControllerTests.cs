ae41b77e5e6da78e717a23b006a5827fda5fa3cf
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-05 18:00:51 +0900

    특성 컨트롤러 테스트 작성

---
+++ Assets/2_Tests/TraitControllerTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TraitControllerTests
+{
+    [Test]
+    public void 챔프_선택하면_특성_가져옴()
+    {
+        TraitController sut = new TraitController();
+
+        // sut.GetChampTriat();
+    }
+}
