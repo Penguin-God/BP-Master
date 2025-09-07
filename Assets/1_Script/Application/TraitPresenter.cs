beed1ab47041d59727cdd1ecafc515aa76f487c9
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-07 22:56:44 +0900

    특성 사용 함수 범죄 통과

--- Assets/1_Script/Application/TraitPresenter.cs
+++ Assets/1_Script/Application/TraitPresenter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,28 +8,11 @@ public class TraitPresenter
     readonly IReadOnlyDictionary<Team, IReadOnlyList<Champion>> teamMembers;
     IReadOnlyList<Champion> allChampion => teamMembers.Values.SelectMany(x => x).ToList();
     Champion selectChamp;
-    Side TargetSide => selectChamp.Trait.TargetSide;
-    TargetRange TargetRange => selectChamp.Trait.TargetRange;
-    public TraitPresenter(Team team, IReadOnlyDictionary<Team, IReadOnlyList<Champion>> teamMembers)
-    {
-        Team = team;
-        this.teamMembers = teamMembers;
-    }
-
-    public IEnumerable<int> GetTargetIds(int id) => GetTargets(id)?.Select(x => x.Id);
-    public IEnumerable<Champion> GetTargets(int id)
-    {
-        if (selectChamp == null) return null;
-
-        Team targetTeam = BanPickEnumCaster.GetTargetTeam(Team, TargetSide);
-        // if (targetTeam == Team.All && targetRange == TraitTargetType.All) return teamMembers.Values.SelectMany(x => x).ToList(); // 당장은 무의미
-        if (teamMembers[targetTeam].Any(x => x.Id == id) == false) return null;
-
-        if (TargetRange == TargetRange.Single) return new Champion[] { FindChampion(id) };
-        else if (TargetRange == TargetRange.All) return teamMembers[targetTeam];
-        return null;
-    }
-    Champion FindChampion(int id) => allChampion.First(x => x.Id == id);
 
     public void SelectChamp(int id) => selectChamp = teamMembers[Team].First(x => x.Id == id);
+
+    public bool UseTrait(int targetIndex)
+    {
+        return false;
+    }
 }
--- Assets/2_Tests/PresenterTests/ActivePresentTests.cs
+++ Assets/2_Tests/PresenterTests/ActivePresentTests.cs
@@ -4,5 +4,11 @@ using System.Linq;
 
 public class ActivePresentTests
 {
+    [Test]
+    public void 챔_선택_안하고_특성_사용시_False()
+    {
+        TraitPresenter traitPresenter = new TraitPresenter();
 
+        Assert.IsFalse(traitPresenter.UseTrait(1));
+    }
 }
