3219a74ed4718ac5ceb289c06f8161915032de71
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-07 21:40:43 +0900

    타게팅 전용 클래스 생성 및 테스트 통과

---
+++ Assets/1_Script/Domain/Trait/TraitTargetFinder.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class TraitTargetFinder
+{
+    readonly IReadOnlyDictionary<Team, IReadOnlyList<int>> teamMembers;
+    IReadOnlyList<int> all_Id => teamMembers.Values.SelectMany(x => x).ToList();
+
+    public TraitTargetFinder(IReadOnlyDictionary<Team, IReadOnlyList<int>> teamMembers) => this.teamMembers = teamMembers;
+
+    public IEnumerable<int> GetTargets(Team targetTeam, TargetRange range, int targetId)
+    {
+        if (teamMembers[targetTeam].Any(x => x == targetId) == false) return null;
+
+        switch (range)
+        {
+            case TargetRange.Single: return new int[] { targetId };
+            case TargetRange.All: return teamMembers[targetTeam];
+            default: return null;
+        }
+    }
+}
---
+++ Assets/2_Tests/Trait/TraitTargetFindingTests.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TraitTargetFindingTests
+{
+    IReadOnlyDictionary<Team, IReadOnlyList<int>> teamMembers;
+    [SetUp]
+    public void SetUp()
+    {
+        teamMembers = new Dictionary<Team, IReadOnlyList<int>>
+        {
+            { Team.Blue, new List<int> { 1, 2 } },
+            { Team.Red,  new List<int> { 11, 12 } }
+        };
+    }
+
+    [Test]
+    public void 잘못된_대산은_null_반환()
+    {
+        var sut = new TraitTargetFinder(teamMembers);
+
+        Assert.IsNull(sut.GetTargets(Team.Red, TargetRange.Single, 2));
+        Assert.IsNull(sut.GetTargets(Team.Blue, TargetRange.All, 12));
+    }
+
+    [Test]
+    public void 싱글은_단일_대상_반환()
+    {
+        var sut = new TraitTargetFinder(teamMembers);
+
+        Assert.AreEqual(2, sut.GetTargets(Team.Blue, TargetRange.Single, 2).First());
+        Assert.AreEqual(11, sut.GetTargets(Team.Red, TargetRange.Single, 11).First());
+    }
+
+    [Test]
+    public void All은_전체_반환()
+    {
+        var sut = new TraitTargetFinder(teamMembers);
+
+        CollectionAssert.AreEqual(new int[] { 1, 2 }, sut.GetTargets(Team.Blue, TargetRange.All, 1));
+        CollectionAssert.AreEqual(new int[] { 11, 12 }, sut.GetTargets(Team.Red, TargetRange.All, 11));
+    }
+}
