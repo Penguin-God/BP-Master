14ea946f2e1b1f01e7181ebdefe8b229ff298b4c
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-08 13:58:38 +0900

    TraitUseView 생성

---
+++ Assets/1_Script/UI/Match/TraitUseView.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TraitUseView : MonoBehaviour
+{
+    [SerializeField] Button[] championButtons;
+    [SerializeField] Button[] targetButtons;
+    
+    TraitPresenter presenter;
+    Team currentTeam;
+    
+    public void Init(TraitPresenter presenter)
+    {
+        this.presenter = presenter;
+        SetupChampionButtons();
+    }
+    public void ChangeTeam(Team team) => currentTeam = team;
+
+    void SetupChampionButtons()
+    {
+        for (int i = 0; i < championButtons.Length; i++)
+        {
+            int index = i; // 클로저 캡처 방지
+            championButtons[i].onClick.AddListener(() => OnChampionButtonClicked(index));
+        }
+    }
+
+    void OnChampionButtonClicked(int index) => presenter.SelectTrait(currentTeam, index);
+
+    void OnTargetButtonClicked(int index)
+    {
+        presenter.UseTrait(index);
+    }
+}
