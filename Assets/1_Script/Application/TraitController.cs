622ce0f4c22b19174806e6d8894174738861df7e
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-11 16:25:37 +0900

    중복 사용 불가 테스트 통과

--- Assets/1_Script/Application/TraitController.cs
+++ Assets/1_Script/Application/TraitController.cs
@@ -12,19 +12,25 @@ public enum TraitType
 public class TraitController
 {
     readonly IReadOnlyDictionary<Team, IReadOnlyList<Champion>> championsByTeam;
+    readonly IReadOnlyDictionary<Team, bool[]> traitUseFlags;
     readonly TraitTargetSelector targetFinder;
     
+
     public TraitController(IReadOnlyDictionary<Team, IReadOnlyList<Champion>> traitsByTeam)
     {
         this.championsByTeam = traitsByTeam;
         targetFinder = new TraitTargetSelector(traitsByTeam[Team.Blue].Count);
+        traitUseFlags = traitsByTeam.ToDictionary(x => x.Key, x => new bool[x.Value.Count]);
     }
 
     public bool UseTrait(Team team, int traitIndex, int targetIndex)
     {
+        if (traitUseFlags[team][targetIndex] == true) return false;
+        
         Trait trait = championsByTeam[team][targetIndex].Trait;
         var targetIds = targetFinder.GetTargetIds(trait.TargetRange, targetIndex);
         ExecuteTrait(trait.TraitAction, targetIds.Select(x => championsByTeam[BanPickEnumCaster.GetTargetTeam(team, trait.TargetSide)][x]));
+        traitUseFlags[team][targetIndex] = true;
         return true;
     }
 
