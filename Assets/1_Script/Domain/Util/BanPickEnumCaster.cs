82400e19cbe08fc6707350557dab712d6ef583c1
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-13 12:57:03 +0900

    target캐스팅 로직 변경

--- Assets/1_Script/Domain/Util/BanPickEnumCaster.cs
+++ Assets/1_Script/Domain/Util/BanPickEnumCaster.cs
@@ -1,21 +1,22 @@
+using System;
+
 public static class BanPickEnumCaster
 {
-    public static Team GetTargetTeam(Team selfTeam, Side side)
+    public static Team GetTargetTeam(Team selfTeam, Side side) => (selfTeam, side) switch
     {
-        if (selfTeam == Team.All || side == Side.All) return Team.All;
+        (Team.Blue, Side.Self) => Team.Blue,
+        (Team.Blue, Side.Opponent) => Team.Red,
+        (Team.Red, Side.Self) => Team.Red,
+        (Team.Red, Side.Opponent) => Team.Blue,
+        (_, Side.All) => Team.All,
 
-        switch (selfTeam)
-        {
-            case Team.Blue: return side == Side.Self ? Team.Blue : Team.Red;
-            case Team.Red: return side == Side.Self ? Team.Red : Team.Blue;
-        }
-        return Team.All;
-    }
+        _ => throw new ArgumentOutOfRangeException(nameof(side), $"없는 조합: {selfTeam}, {side}")
+    };
 
     public static SelectType PhaseToSelect(GamePhase phase)
     {
         if (phase == GamePhase.Pick) return SelectType.Pick;
         else if (phase == GamePhase.Ban) return SelectType.Ban;
-        else throw new System.Exception($"밴과 픽 중 하나여야 함 : {phase}");
+        else throw new Exception($"밴과 픽 중 하나여야 함 : {phase}");
     }
 }
