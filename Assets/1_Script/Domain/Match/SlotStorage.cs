7497f2e61134b8da11aad6336703829a6b609ef2
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-17 23:29:30 +0900

    팀별 조회 테스트 통과

--- Assets/1_Script/Domain/Match/SlotStorage.cs
+++ Assets/1_Script/Domain/Match/SlotStorage.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public class SlotStorage
 {
@@ -8,4 +9,10 @@ public class SlotStorage
     public void AddSlot(Team team, Champion champion) => slots.Add(new SlotData(team, indexr.AllocateIndex(team)), champion);
 
     public Champion GetSlot(SlotData slot) => slots[slot];
+
+    public IEnumerable<Champion> GetTeam(Team team) 
+        => slots
+            .Where(kv => kv.Key.Team == team)
+            .OrderBy(kv => kv.Key.Index)
+            .Select(kv => kv.Value);
 }
--- Assets/2_Tests/GameManagment/SlotManagementTests.cs
+++ Assets/2_Tests/GameManagment/SlotManagementTests.cs
@@ -12,4 +12,17 @@ public class SlotManagementTests
 
         Assert.AreEqual(10, sut.GetSlot(TestHelper.CreateBlueSlot(0)).StatData.Attack);
     }
+
+    [Test]
+    public void 팀별_조회()
+    {
+        SlotStorage sut = new();
+
+        var champ1 = TestHelper.CreateStatChamp(10);
+        var champ2 = TestHelper.CreateStatChamp(20);
+        sut.AddSlot(Team.Blue, champ1);
+        sut.AddSlot(Team.Blue, champ2);
+
+        CollectionAssert.AreEqual(new Champion[] { champ1, champ2 }, sut.GetTeam(Team.Blue));
+    }
 }
