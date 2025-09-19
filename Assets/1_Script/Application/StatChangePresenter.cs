c9c0c345dc82c2278433ee76d2b098ceb61590e6
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-19 14:46:41 +0900

    스탯 변화 프레젠터 테스트 제작 및 통과

---
+++ Assets/1_Script/Application/StatChangePresenter.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StatDeltaViewModel
+{
+    public bool IsChange { get; }
+    public Color DeltaTextColor { get; }
+    public string DeltaText { get; }
+    public IEnumerable<int> DeltaValues { get; }
+
+    public StatDeltaViewModel(bool isChange, Color deltaTextColor, string deltaText, IEnumerable<int> deltaValues)
+    {
+        IsChange = isChange;
+        DeltaTextColor = deltaTextColor;
+        DeltaText = deltaText;
+        DeltaValues = deltaValues;
+    }
+}
+
+public class StatChangeViewModel
+{
+    public StatDeltaViewModel Attack { get; }
+    public StatDeltaViewModel Defense { get; }
+    public StatDeltaViewModel Speed { get; }
+
+    public StatChangeViewModel(StatDeltaViewModel attack, StatDeltaViewModel defense, StatDeltaViewModel speed)
+    {
+        Attack = attack;
+        Defense = defense;
+        Speed = speed;
+    }
+}
+
+public class StatChangePresenter
+{
+    readonly Color positiveColor;
+    readonly Color negativeColor;
+
+    public StatChangePresenter(Color positiveColor, Color negativeColor)
+    {
+        this.positiveColor = positiveColor;
+        this.negativeColor = negativeColor;
+    }
+
+    public StatChangeViewModel CreateViewModel(StatChangeData data)
+    {
+        var attackView = BuildDeltaView(data.Before.Attack, data.After.Attack);
+        var defenseView = BuildDeltaView(data.Before.Defense, data.After.Defense);
+        var speedView = BuildDeltaView(data.Before.Speed, data.After.Speed);
+
+        return new StatChangeViewModel(attackView, defenseView, speedView);
+    }
+
+    StatDeltaViewModel BuildDeltaView(int before, int after)
+    {
+        int delta = after - before;
+        if (delta > 0)
+        {
+            var deltaValues = Enumerable.Range(before + 1, delta);
+            return new StatDeltaViewModel(true, positiveColor, $"+{delta}", deltaValues);
+        }
+        else if (delta < 0)
+        {
+            int count = Mathf.Abs(delta);
+            var deltaValues = Enumerable.Range(after, count).Reverse(); // 내림차순
+            return new StatDeltaViewModel(true, negativeColor, $"-{count}", deltaValues);
+        }
+        else return new StatDeltaViewModel(false, Color.white, string.Empty, null);
+    }
+}
