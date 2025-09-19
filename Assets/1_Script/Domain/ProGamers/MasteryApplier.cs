50fc4146d3e3a5f4d231339fae74a96813709a37
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-19 23:01:51 +0900

    마스터리 적용 bool 반환

--- Assets/1_Script/Domain/ProGamers/MasteryApplier.cs
+++ Assets/1_Script/Domain/ProGamers/MasteryApplier.cs
@@ -2,14 +2,16 @@
 
 public class MasteryApplier
 {
-    public void ApplyMastery(ProGamer gamer, Champion champion)
+    public bool ApplyMastery(ProGamer gamer, Champion champion)
     {
         int level = gamer.GetMastery(champion.Id);
+        if (level == 0) return false;
 
         var newStat = champion.StatData
             .ChangeAttack(champion.StatData.Attack + level)
             .ChangeDefense(champion.StatData.Defense + level);
 
         champion.ChangeStat(newStat);
+        return true;
     }
 }
--- Assets/2_Tests/ProGamer/MasteryApplyTests.cs
+++ Assets/2_Tests/ProGamer/MasteryApplyTests.cs
@@ -10,13 +10,13 @@ public class MasteryApplyTests
         var champ2 = new Champion(2, "", new ChampionStatData(10, 5, 0), default, null);
         MasteryApplier sut = new();
 
-        // action
-        sut.ApplyMastery(gamer, champ1);
-        sut.ApplyMastery(gamer, champ2);
-
+        // 숙련도 적용
+        Assert.IsTrue(sut.ApplyMastery(gamer, champ1));
         Assert.AreEqual(20, champ1.StatData.Attack);
         Assert.AreEqual(15, champ1.StatData.Defense);
 
+        // 숙련도 없어서 스탯 그대로
+        Assert.IsFalse(sut.ApplyMastery(gamer, champ2));
         Assert.AreEqual(10, champ2.StatData.Attack);
         Assert.AreEqual(5, champ2.StatData.Defense);
     }
