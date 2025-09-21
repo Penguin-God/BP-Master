35b2a60001d46e7dbfc9734dfe8a101687fbbfc0
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-21 14:36:38 +0900

    숙련도 프레젠터 테스트 생성

---
+++ Assets/2_Tests/Presenter/MasteryPresentTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class MasteryPresentTests
+{
+    //[Test]
+    //public void 숙련도_텍스트로_변환()
+    //{
+    //    MasteryPersenter sut = new();
+    //    Dictionary<int, int> 
+
+    //    string result = sut.Present();
+
+    //    Assert.AreEqual();
+    //}
+}
