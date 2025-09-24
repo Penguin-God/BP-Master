d69c2f473e61e5182657fcdf195a94d1e85b910f
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-24 17:44:10 +0900

    죽은코드 제거

--- Assets/1_Script/Domain/Match/SlotStorage.cs
+++ Assets/1_Script/Domain/Match/SlotStorage.cs
@@ -6,19 +6,6 @@ public class SlotStorage<T>
     readonly TeamSlotIndexr indexr = new TeamSlotIndexr();
     readonly Dictionary<SlotData, T> slots = new();
 
-    public SlotStorage() { }
-
-    // 슬롯 수와 기본값으로 초기화 (Blue 팀 기준)
-    public SlotStorage(int slotCount, T value)
-    {
-        foreach (var team in new[] { Team.Blue, Team.Red })
-        {
-            Enumerable.Range(0, slotCount)
-                .ToList()
-                .ForEach(_ => AddSlot(team, value));
-        }
-    }
-
     public void AddSlot(Team team, T value) => slots.Add(new SlotData(team, indexr.AllocateIndex(team)), value);
     public void AddSlots(Team team, IEnumerable<T> items)
     {
