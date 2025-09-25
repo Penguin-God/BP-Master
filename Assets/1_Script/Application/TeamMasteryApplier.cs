a38d8565bef73af54f4aa935af3ee66ab28a692b
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-26 00:01:12 +0900

    죽은 코드 제거

--- Assets/1_Script/Application/TeamMasteryApplier.cs
+++ Assets/1_Script/Application/TeamMasteryApplier.cs
@@ -1,43 +1,9 @@
 using System.Collections.Generic;
-using System.Linq;
 
 public class TeamMasteryApplier
 {
-    readonly SlotStorage<ProGamer> gamers;
-    readonly SlotStorage<int> ids;
     readonly SlotStatusChanger statusChanger;
-
-    public TeamMasteryApplier(SlotStorage<ProGamer> gamers, SlotStorage<int> ids, SlotStatusChanger statusChanger)
-    {
-        this.gamers = gamers;
-        this.ids = ids;
-        this.statusChanger = statusChanger;
-    }
-
-    public void Apply(Team team)
-    {
-        var finder = new ActiveMasteryFinder(gamers, ids);
-
-        // 사용자의 스타일(반복문 대신 LINQ 지향)에 맞춰 인덱스 포함 열거
-        gamers.GetTeam(team)
-              .Select((_, index) => new SlotData(team, index))
-              .ToList()
-              .ForEach(slot =>
-              {
-                  int level = finder.GetActiveLevel(slot);
-                  statusChanger.ChangeStat(slot, stat => new MasteryApplier().ApplyMastery(stat, level));
-              });
-    }
-}
-
-public class TeamMasteryApplier2
-{
-    readonly SlotStatusChanger statusChanger;
-
-    public TeamMasteryApplier2(SlotStatusChanger statusChanger)
-    {
-        this.statusChanger = statusChanger;
-    }
+    public TeamMasteryApplier(SlotStatusChanger statusChanger) => this.statusChanger = statusChanger;
 
     public void Apply(IEnumerable<PickSlotData> datas)
     {
