9cbb3e947ff6ba1cc62e70fe3884667be5d155cb
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-29 18:21:45 +0900

    특성 적용 테스트 퍼사드에서 적용자로 옮김

--- Assets/2_Tests/Application/Trait/TraitApplyTests.cs
+++ Assets/2_Tests/Application/Trait/TraitApplyTests.cs
@@ -7,21 +7,18 @@ public class TraitApplyTests
     public void 조건은_실시간_반영()
     {
         SlotStorage<ChampionStatus> statuses = new();
-        // Blue 2, Red 2 상태 초기화 (공격력 0)
+        // Blue 1, Red 1 상태 초기화 (공격력 0)
         statuses.AddSlot(Team.Blue, CreateStatus(0));
-        statuses.AddSlot(Team.Blue, CreateStatus(0));
-        statuses.AddSlot(Team.Red, CreateStatus(0));
         statuses.AddSlot(Team.Red, CreateStatus(0));
 
+        var traitData = CreateAttTraitData(15, TraitConditionType.AttackBelow, 10, range: TargetRange.All);
         TraitApplier sut = new TraitApplier(statuses);
 
-        //Assert.IsTrue(sut.Execute(CreateBlueSlot(0), CreateRedSlot(0), CreateTraitData(TraitType.AttackChanger, 15, TraitConditionType.AttackBelow, 10), TargetRange.All));
-        //Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(0)).Stat.Attack);
-        //Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(1)).Stat.Attack);
+        sut.Execute(traitData, CreateRedSlot(0));
+        Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(0)).Stat.Attack);
 
-        //// 사용은 되지만 조건이 안되서 적용 안됨
-        //Assert.IsTrue(sut.UseTrait(CreateBlueSlot(1), CreateRedSlot(0), CreateTraitData(TraitType.AttackChanger, 15, TraitConditionType.AttackBelow, 10), TargetRange.All));
-        //Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(0)).Stat.Attack);
-        //Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(1)).Stat.Attack);
+        // 조건이 거짓이므로 아무일도 안생김
+        sut.Execute(traitData, CreateRedSlot(0));
+        Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(0)).Stat.Attack);
     }
 }
--- Assets/2_Tests/Application/Trait/TraitFacadeTests.cs
+++ Assets/2_Tests/Application/Trait/TraitFacadeTests.cs
@@ -20,26 +20,4 @@ public class TraitFacadeTests
         Assert.IsFalse(sut.UseTrait(CreateBlueSlot(0), CreateRedSlot(0), null));
         Assert.AreEqual(10, statuses.GetSlot(CreateRedSlot(0)).Stat.Attack);
     }
-
-    [Test]
-    public void 조건은_실시간_반영()
-    {
-        SlotStorage<ChampionStatus> statuses = new();
-        // Blue 2, Red 2 상태 초기화 (공격력 0)
-        statuses.AddSlot(Team.Blue, CreateStatus(0));
-        statuses.AddSlot(Team.Blue, CreateStatus(0));
-        statuses.AddSlot(Team.Red, CreateStatus(0));
-        statuses.AddSlot(Team.Red, CreateStatus(0));
-
-        TraitUseFacade sut = new TraitUseFacade(statuses);
-
-        Assert.IsTrue(sut.UseTrait(CreateBlueSlot(0), CreateRedSlot(0), CreateAttTraitData(15, TraitConditionType.AttackBelow, 10, range: TargetRange.All)));
-        Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(0)).Stat.Attack);
-        Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(1)).Stat.Attack);
-
-        // 사용은 되지만 조건이 안되서 적용 안됨
-        Assert.IsTrue(sut.UseTrait(CreateBlueSlot(1), CreateRedSlot(0), CreateAttTraitData(15, TraitConditionType.AttackBelow, 10, range: TargetRange.All)));
-        Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(0)).Stat.Attack);
-        Assert.AreEqual(15, statuses.GetSlot(CreateRedSlot(1)).Stat.Attack);
-    }
 }
