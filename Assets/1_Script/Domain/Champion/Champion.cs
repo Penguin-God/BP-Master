8847e1d282f3422bc97748db73488d4eb788ccab
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-09-30 15:10:21 +0900

    stat 필드랑 매서드 readonly 추가

--- Assets/1_Script/Domain/Champion/Champion.cs
+++ Assets/1_Script/Domain/Champion/Champion.cs
@@ -1,8 +1,8 @@
-public struct ChampionStatData
+public readonly struct ChampionStatData
 {
-    public int Attack { get; private set; }
-    public int Defense { get; private set; }
-    public int Speed { get; private set; }
+    public readonly int Attack;
+    public readonly int Defense;
+    public readonly int Speed;
     public ChampionStatData(int attack, int defense, int speed)
     {
         Attack = MinZero(attack);
@@ -12,9 +12,9 @@ public struct ChampionStatData
 
     static int MinZero(int value) => 0 > value ? 0 : value;
 
-    public ChampionStatData ChangeAttack(int att) => new ChampionStatData(att, Defense, Speed);
-    public ChampionStatData ChangeDefense(int def) => new ChampionStatData(Attack, def, Speed);
-    public ChampionStatData ChangeSpeed(int speed) => new ChampionStatData(Attack, Defense, speed);
+    public readonly ChampionStatData ChangeAttack(int att) => new ChampionStatData(att, Defense, Speed);
+    public readonly ChampionStatData ChangeDefense(int def) => new ChampionStatData(Attack, def, Speed);
+    public readonly ChampionStatData ChangeSpeed(int speed) => new ChampionStatData(Attack, Defense, speed);
 }
 
 public class Champion
