05dddc7e236fe544749357f58abb812a572cc664
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-12 13:08:00 +0900

    useSlot추가 및 사용

--- Assets/1_Script/Application/Persenters/TraitSelectionState.cs
+++ Assets/1_Script/Application/Persenters/TraitSelectionState.cs
@@ -7,18 +7,26 @@ public enum TraitSelectResult
 
 public class TraitSelectionState
 {
-    public bool IsSelect { get; private set; }
-
     public readonly Team Team;
+    SlotData? useSlot;
+    public SlotData UseSlot => useSlot.Value;
+    public bool IsSelect => useSlot.HasValue;
     public TraitSelectionState(Team team) => Team = team;
 
     public TraitSelectResult SelectTraitSlot(SlotData slot)
     {
         if (IsSelectable(slot.Team) == false) return TraitSelectResult.Faild;
 
-        var result = IsSelect ? TraitSelectResult.Use : TraitSelectResult.Select;
-        IsSelect = !IsSelect;
-        return result;
+        if (IsSelect)
+        {
+            useSlot = null;
+            return TraitSelectResult.Use;
+        }
+        else
+        {
+            useSlot = slot;
+            return TraitSelectResult.Select;
+        }
     }
 
     bool IsSelectable(Team selectTeam) => (IsSelect == false && Team == selectTeam) || IsSelect;
