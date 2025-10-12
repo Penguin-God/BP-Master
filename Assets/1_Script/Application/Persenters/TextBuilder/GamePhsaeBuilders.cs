d24675c4f4b874326ff40c2eaf767576b95ec26c
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-12 23:24:40 +0900

    게임 진행 텍스트 빌더 제작

---
+++ Assets/1_Script/Application/Persenters/TextBuilder/GamePhsaeBuilders.cs
@@ -0,0 +1,30 @@
+
+public class GameFlowTextBuilder
+
+{
+    public string BuildFlowText(GameFlowData flow) => $"{BuildTurnText(flow.Turn)} {BuildPhaseText(flow.Phase)} 단계";
+
+    string BuildPhaseText(GamePhase phase)
+    {
+        switch (phase)
+        {
+            case GamePhase.Ban: return "밴";
+            case GamePhase.Pick: return "픽";
+            case GamePhase.Swap: return "스왑";
+            case GamePhase.Trait: return "특성";
+            case GamePhase.Done: return "끝";
+            default: return "";
+        }
+    }
+
+    string BuildTurnText(Team team)
+    {
+        switch (team)
+        {
+            case Team.Blue: return "파랑 팀";
+            case Team.Red: return "빨강 팀";
+            case Team.All: return "양팀";
+            default: return "";
+        }
+    }
+}
---
+++ Assets/2_Tests/Application/PresenterTests/GameFlowTextBuilderTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class GameFlowTextBuilderTests
+{
+    [TestCase(GamePhase.Ban, Team.Blue, "파랑 팀 밴 단계")]
+    [TestCase(GamePhase.Pick, Team.Red, "빨강 팀 픽 단계")]
+    [TestCase(GamePhase.Swap, Team.All, "양팀 스왑 단계")]
+    [TestCase(GamePhase.Trait, Team.Blue, "파랑 팀 특성 단계")]
+    [TestCase(GamePhase.Done, Team.Red, "빨강 팀 끝 단계")]
+    public void 각_단계와_팀_조합에_맞는_텍스트_생성(GamePhase phase, Team team, string expected)
+    {
+        var sut = new GameFlowTextBuilder();
+        var flow = new GameFlowData(phase, team);
+
+        var result = sut.BuildFlowText(flow);
+
+        Assert.AreEqual(expected, result);
+    }
+}
