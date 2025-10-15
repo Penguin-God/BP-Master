2b762265f8d9d48a14c1c675730ebaf55b9df419
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-16 00:08:47 +0900

    ai 테스트 수정

--- Assets/2_Tests/Application/AI_TraitUseTests.cs
+++ Assets/2_Tests/Application/AI_TraitUseTests.cs
@@ -16,14 +16,18 @@ public class AI_TraitUseTests
         traitStorage.AddSlot(Team.Blue, traits);
         traitStorage.AddSlot(Team.Red, traits);
 
-        var facade = new TraitUseFacade(statuses);
-        var filter = new TraitSlotFilter(teamSize: 1, facade);
+        
+        SlotStorage<TraitApplier> applilers = new();
+        applilers.AddSlot(Team.Blue, new TraitApplier(statuses));
+        applilers.AddSlot(Team.Red, new TraitApplier(statuses));
 
+        var facade = new TraitUseFacade(applilers);
+        var filter = new TraitSlotFilter(applilers);
         var sut = new AI_TraitAgent(Team.Blue, filter, traitStorage, facade);
 
         sut.UseTrait(Team.Blue);
 
-        Assert.IsTrue(statuses.GetSlot(TestHelper.CreateBlueSlot(0)).IsUseTrait);
+        Assert.IsTrue(applilers.GetSlot(TestHelper.CreateBlueSlot(0)).IsUse);
         Assert.AreEqual(10, statuses.GetSlot(TestHelper.CreateRedSlot(0)).Stat.Attack);
     }
 }
