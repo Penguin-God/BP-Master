8a324f33e1b5323ff5381528ee22e2de96ae5ca3
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-17 16:36:02 +0900

    테스트 중복 제거

--- Assets/2_Tests/Domain/Trait/TraitConditionTests.cs
+++ Assets/2_Tests/Domain/Trait/TraitConditionTests.cs
@@ -3,69 +3,31 @@ using NUnit.Framework;
 public class TraitConditionTests
 {
     [Test]
-    public void None은_무조건_참()
-    {
-        Assert.IsTrue(Check(TraitConditionType.None, default, 0));
-    }
+    public void None은_무조건_참() => Assert.IsTrue(Check(TraitConditionType.None, default, 0));
 
     [Test]
-    public void 방어력이_기준점_이하면_참()
+    [TestCase(TraitConditionType.AttackBelow)]
+    [TestCase(TraitConditionType.DefenseBelow)]
+    [TestCase(TraitConditionType.SpeedBelow)]
+    public void 스탯이_기준_이하면_참(TraitConditionType type)
     {
         int threshold = 100;
 
-        Assert.IsTrue(Check(TraitConditionType.DefenseBelow, default, threshold));
-        Assert.IsTrue(Check(TraitConditionType.DefenseBelow, Stat(def: 100), threshold));
-        Assert.IsFalse(Check(TraitConditionType.DefenseBelow, Stat(def: 120), threshold));
+        Assert.IsTrue(Check(type, default, threshold));
+        Assert.IsTrue(Check(type, Stat(100, 100, 100), threshold));
+        Assert.IsFalse(Check(type, Stat(120, 120, 120), threshold));
     }
 
     [Test]
-    public void 방어력이_기준점_이상이면_참()
+    [TestCase(TraitConditionType.AttackAtLeast)]
+    [TestCase(TraitConditionType.DefenseAtLeast)]
+    [TestCase(TraitConditionType.SpeedAtLeast)]
+    public void 스탯이_기준_이상이면_참(TraitConditionType type)
     {
         int threshold = 100;
 
-        Assert.IsFalse(Check(TraitConditionType.DefenseAtLeast, Stat(def: 50), threshold));
-        Assert.IsTrue(Check(TraitConditionType.DefenseAtLeast, Stat(def: 100), threshold));
-        Assert.IsTrue(Check(TraitConditionType.DefenseAtLeast, Stat(def: 120), threshold));
-    }
-
-    [Test]
-    public void 공격력이_기준점_이하면_참()
-    {
-        int threshold = 50;
-
-        Assert.IsTrue(Check(TraitConditionType.AttackBelow, default, threshold));
-        Assert.IsTrue(Check(TraitConditionType.AttackBelow, Stat(att: 50), threshold));
-        Assert.IsFalse(Check(TraitConditionType.AttackBelow, Stat(att: 70), threshold));
-    }
-
-    [Test]
-    public void 공격력이_기준점_이상이면_참()
-    {
-        int threshold = 50;
-
-        Assert.IsFalse(Check(TraitConditionType.AttackAtLeast, Stat(att: 30), threshold));
-        Assert.IsTrue(Check(TraitConditionType.AttackAtLeast, Stat(att: 50), threshold));
-        Assert.IsTrue(Check(TraitConditionType.AttackAtLeast, Stat(att: 70), threshold));
-    }
-
-    [Test]
-    public void 속도가_기준점_이하면_참()
-    {
-        int threshold = 10;
-
-        Assert.IsTrue(Check(TraitConditionType.SpeedBelow, default, threshold));
-        Assert.IsTrue(Check(TraitConditionType.SpeedBelow, Stat(speed: 10), threshold));
-        Assert.IsFalse(Check(TraitConditionType.SpeedBelow, Stat(speed: 20), threshold));
-    }
-
-    [Test]
-    public void 속도가_기준점_이상이면_참()
-    {
-        int threshold = 10;
-
-        Assert.IsFalse(Check(TraitConditionType.SpeedAtLeast, Stat(speed: 5), threshold));
-        Assert.IsTrue(Check(TraitConditionType.SpeedAtLeast, Stat(speed: 10), threshold));
-        Assert.IsTrue(Check(TraitConditionType.SpeedAtLeast, Stat(speed: 20), threshold));
+        Assert.IsTrue(Check(type, Stat(110, 120, 100), threshold));
+        Assert.IsFalse(Check(type, Stat(80, 80, 80), threshold));
     }
 
     [Test]
