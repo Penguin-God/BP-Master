5d2f080d08284ec3ff025b6c83a92cb83dc797c3
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-24 19:08:49 +0900

    테스트에서 new 생성자 함수 호출로 대체

--- Assets/2_Tests/Application/TeamMasteryApplierTests.cs
+++ Assets/2_Tests/Application/TeamMasteryApplierTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using static TestHelper;
 
 public class TeamMasteryApplier_GamersIds_Tests
 {
@@ -9,8 +10,8 @@ public class TeamMasteryApplier_GamersIds_Tests
     public void Setup()
     {
         statusTable = new SlotStorage<ChampionStatus>();
-        statusTable.AddSlot(Team.Blue, new ChampionStatus(new ChampionStatData(10, 20, 30)));
-        statusTable.AddSlot(Team.Red, new ChampionStatus(new ChampionStatData(40, 50, 60)));
+        statusTable.AddSlot(Team.Blue, CreateStatus(10, 20, 30));
+        statusTable.AddSlot(Team.Red, CreateStatus(40, 50, 60));
 
         sut = new TeamMasteryApplier();
     }
@@ -21,14 +22,11 @@ public class TeamMasteryApplier_GamersIds_Tests
         var gamers = new SlotStorage<ProGamer>();
         var blueMasteries = new[] { new ChampionMastery(1, 3) };
         gamers.AddSlot(Team.Blue, new ProGamer(blueMasteries));
-
         var ids = new SlotStorage<int>();
         ids.AddSlot(Team.Blue, 1);
 
-        // Action
         sut.Apply(gamers, ids, statusTable);
 
-        // Assert
         var result = statusTable.GetSlot(new SlotData(Team.Blue, 0)).Stat;
         Assert.AreEqual(new ChampionStatData(13, 23, 30), result);
     }
--- Assets/2_Tests/Domain/ChampionTests/ChampionStatusTests.cs
+++ Assets/2_Tests/Domain/ChampionTests/ChampionStatusTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using static TestHelper;
 
 [TestFixture]
 public class ChampionStatusTests
@@ -6,9 +7,7 @@ public class ChampionStatusTests
     [Test]
     public void 생성시_스탯이_정상적으로_저장된다()
     {
-        var stat = new ChampionStatData(attack: 10, defense: 5, speed: 3);
-
-        var status = new ChampionStatus(stat);
+        var status = CreateStatus(att: 10, def: 5, speed: 3);
 
         Assert.AreEqual(10, status.Stat.Attack);
         Assert.AreEqual(5, status.Stat.Defense);
@@ -18,16 +17,16 @@ public class ChampionStatusTests
     [Test]
     public void ChangeStat_호출시_스탯이_변경_후_이벤트()
     {
-        var status = new ChampionStatus(new ChampionStatData(1, 1, 1));
+        var status = CreateStatus(1, 1, 1);
         ChampionStatData before = default;
         ChampionStatData after = default;
         status.OnStatChanged += (be, af) => (before, after) = (be, af);
-        var newStat = new ChampionStatData(5, 6, 7);
+        var newStat = CreateStat(5, 6, 7);
 
         status.ChangeStat(newStat);
 
         Assert.AreEqual(newStat, status.Stat);
-        Assert.AreEqual(new ChampionStatData(1, 1, 1), before);
+        Assert.AreEqual(CreateStat(1, 1, 1), before);
         Assert.AreEqual(newStat, after);
     }
 }
