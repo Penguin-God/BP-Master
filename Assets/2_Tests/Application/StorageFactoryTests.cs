2fe482997867ed0a690c3c960e29331c28faaed5
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-25 22:51:24 +0900

    변환 테스트 추가

--- Assets/2_Tests/Application/StorageFactoryTests.cs
+++ Assets/2_Tests/Application/StorageFactoryTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System.Linq;
 using static TestHelper;
 
 public class StorageFactoryTests
@@ -24,5 +23,18 @@ public class StorageFactoryTests
 
         Assert.AreEqual(new ChampionStatData(10, 10, 10), result.GetSlot(CreateBlueSlot(0)).Stat);
         Assert.AreEqual(new ChampionStatData(20, 20, 20), result.GetSlot(CreateRedSlot(0)).Stat);
+    }
+
+    [Test]
+    public void 슬롯을_함수를_통해_변환()
+    {
+        SlotStorage<int> data = new SlotStorage<int>();
+        data.AddSlots(Team.Red, new int[] { 1, 2 });
+        data.AddSlots(Team.Blue, new int[] { 1, 2 });
+
+        var result = StorageConverter.ConvertStorage(data, number => number.ToString());
+
+        CollectionAssert.AreEqual(new string[] { "1", "2" }, result.GetTeam(Team.Blue));
+        CollectionAssert.AreEqual(new string[] { "1", "2" }, result.GetTeam(Team.Red));
     }
 }
