aec3c8b4afbbb673a8ecb5d7cf9241ef3c584ca4
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-25 22:48:27 +0900

    죽은 변환 코드 제거

--- Assets/1_Script/Application/IdStorageConverter.cs
+++ Assets/1_Script/Application/IdStorageConverter.cs
@@ -1,22 +1,11 @@
 using System;
-using System.Collections.Generic;
 
 public class IdStorageConverter
 {
     readonly ChampionCatalog championCatalog;
     public IdStorageConverter(ChampionCatalog championCatalog) => this.championCatalog = championCatalog;
 
-    public SlotStorage<ChampionStatus> IdToStatus(SlotStorage<int> idStorage)
-        => StorageConverter.ConvertStorage(idStorage, id => new ChampionStatus(championCatalog.GetChampion(id).StatData, TraitType.Charge));
-
-    public SlotStorage<Champion> IdToChampion(SlotStorage<int> idStorage)
-        => StorageConverter.ConvertStorage(idStorage, id => championCatalog.GetChampion(id));
-}
-
-public static class ChampionStorageConverter
-{
-    public static SlotStorage<IEnumerable<SkillData>> ChamptionToSkill(SlotStorage<Champion> champions)
-        => StorageConverter.ConvertStorage(champions, champ => champ.TraitDatas);
+    public SlotStorage<ChampionStatus> IdToStatus(SlotStorage<int> idStorage) => StorageConverter.ConvertStorage(idStorage, id => new ChampionStatus(championCatalog.GetChampion(id).StatData, TraitType.Charge));
 }
 
 public static class StorageConverter
--- Assets/2_Tests/Application/StorageFactoryTests.cs
+++ Assets/2_Tests/Application/StorageFactoryTests.cs
@@ -25,29 +25,4 @@ public class StorageFactoryTests
         Assert.AreEqual(new ChampionStatData(10, 10, 10), result.GetSlot(CreateBlueSlot(0)).Stat);
         Assert.AreEqual(new ChampionStatData(20, 20, 20), result.GetSlot(CreateRedSlot(0)).Stat);
     }
-
-    [Test]
-    public void ID를_챔피언으로_변환()
-    {
-        SlotStorage<Champion> result = sut.IdToChampion(data);
-
-        Assert.AreEqual("일", result.GetSlot(CreateBlueSlot(0)).Name);
-        Assert.AreEqual("이", result.GetSlot(CreateRedSlot(0)).Name);
-    }
-
-    [Test]
-    public void 챔피언을_특성_데이터로_반환()
-    {
-        var championStorage = new SlotStorage<Champion>();
-        championStorage.AddSlots(Team.Blue, new[] { CreateTraitChamp(Side.All, TargetRange.All, 10), });
-        championStorage.AddSlots(Team.Red, new[] { CreateTraitChamp(Side.All, TargetRange.All, 50), });
-
-        // Act
-        var result = ChampionStorageConverter.ChamptionToSkill(championStorage);
-
-        // Assert
-        Assert.AreEqual(1, result.GetTeam(Team.Blue).Count());
-        Assert.AreEqual(1, result.GetTeam(Team.Red).Count());
-        Assert.AreEqual(10, result.GetSlot(CreateBlueSlot(0)).ToArray()[0].Amount);
-    }
 }
