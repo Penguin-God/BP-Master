cb1aea91b9d50b1222e124c56727dba28e352d2a
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-26 18:43:03 +0900

    MatchConfigSO 생성

---
+++ Assets/1_Script/Unity/SO/MatchConfigSO.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MatchConfigSO", menuName = "BP Master/MatchConfigSO")]
+public class MatchConfigSO : ScriptableObject
+{
+    [Header("기본 설정")]
+    [SerializeField] int teamSize;
+    public int TeamSize => teamSize;
+
+    [Header("특성 보너스 설정")]
+    [SerializeField] int chargeAttackBonus;
+    public int ChargeAttackBonus => chargeAttackBonus;
+
+    [SerializeField] float guardBonusRate;
+    public float GuardBonusRate => guardBonusRate;
+
+    [SerializeField] float amplifyBonusRate;
+    public float AmplifyBonusRate => amplifyBonusRate;
+}
