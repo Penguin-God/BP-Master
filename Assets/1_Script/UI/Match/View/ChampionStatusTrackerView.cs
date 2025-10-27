61698366ef9663f02ab7132c10fb029e01fd596e
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-27 20:45:45 +0900

    ChampionStatusTrackerView 추가

---
+++ Assets/1_Script/UI/Match/View/ChampionStatusTrackerView.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+
+public class ChampionStatusTrackerView : MonoBehaviour
+{
+    [SerializeField] StatChangeView statChangeView;
+    [SerializeField] TextMeshProUGUI increaseRateText;
+    [SerializeField] TextMeshProUGUI decreaseRateText;
+
+    ChampionStatus target;
+    readonly ChampionStatusTextBuilder StatusTextBuilder = new ChampionStatusTextBuilder();
+    public void Init(ChampionStatus target)
+    {
+        this.target = target;
+        target.OnStatChanged += (be, af) => statChangeView.ChangeStat(new StatChangeData(be, af));
+    }
+
+    void Update()
+    {
+        var combatModifierTextModel = StatusTextBuilder.BuildCombatModel(target.UpRate, target.DownRate);
+        increaseRateText.text = combatModifierTextModel.IncreaseRateText;
+        decreaseRateText.text = combatModifierTextModel.DecreaseRateText;
+    }
+}
