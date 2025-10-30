60e9b77a881062f88b615913d26fbc905a316110
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-10-30 17:53:25 +0900

    ai폴더 구조변경

---
+++ Assets/2_Tests/Application/AI/SelectChampsTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class SelectChampsTests
+{
+    [Test]
+    public void 가장_가치가_높은_챔피언_픽()
+    {
+        ChampionCatalog catalog = new ChampionCatalog(new Dictionary<int, ChampionData>()
+        {
+            {1, Champ(10, 20) },
+            {2, Champ(10, 50) },
+            {3, Champ(100, 50) },
+        });
+        StaticValueEvaluator evaluator = new StaticValueEvaluator(new ChampionMastery[0]);
+        StaticValuePick sut = new StaticValuePick(catalog, evaluator);
+
+        int result = sut.Pick(new HashSet<int>() { 1, 2, 3 });
+
+        Assert.AreEqual(3, result);
+    }
+
+    ChampionData Champ(int att, int def) => new ChampionData(TestHelper.CreateStat(att, def), TraitType.None, default);
+}
