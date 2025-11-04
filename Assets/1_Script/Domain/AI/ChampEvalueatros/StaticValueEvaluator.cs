6e09749e743c87bf204ea1cfb673112d71ce27cf
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-11-04 15:27:02 +0900

    TODO

--- Assets/1_Script/Domain/AI/ChampEvalueatros/StaticValueEvaluator.cs
+++ Assets/1_Script/Domain/AI/ChampEvalueatros/StaticValueEvaluator.cs
@@ -21,20 +21,11 @@ public class StaticValueEvaluator
 public class SkillEvaluator
 {
     SlotStorage<ChampionStatus> statuses;
+    readonly int TeamSize;
     public SkillEvaluator(SlotStorage<ChampionStatus> statuses) => this.statuses = statuses;
 
     public int Evaluate(SkillData skill, Team team)
     {
-        if (skill.TargetRule.TargetSide == Side.All) return 0;
-
-        int result = statuses.GetTeamCount(Team.Blue) * skill.Amount;
-        if (skill.TargetRule.TargetSide == Side.Opponent) result *= -1;
-        return result;
-    }
-
-    public int Evaluate(SkillExecutor executor, Team team)
-    {
-        // var targets = new SkillTargetFinder(statuses.GetTeamCount(team)).GetTargetSlots();
         if (skill.TargetRule.TargetSide == Side.All) return 0;
 
         int result = statuses.GetTeamCount(Team.Blue) * skill.Amount;
