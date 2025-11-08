1dd231f56191649f8ff7c551abecfe78896ba4df
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-11-08 19:35:32 +0900

    테스트 통과

--- Assets/1_Script/Domain/AI/AI_SKillDicision.cs
+++ Assets/1_Script/Domain/AI/AI_SKillDicision.cs
@@ -1,6 +1,30 @@
-using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class AI_SKillDicision
 {
-    
+    public Skill SelectSkill(IEnumerable<Skill> skills)
+    {
+        var list = skills.ToList();
+
+        var first = list.Where(s => s.IsFreeCondition && HasType(s, SkillType.AttackChanger, SkillType.DefenseChanger)).ToList();
+        if (first.Count > 0)
+            return RandomUtil.DrawRandom(first);
+
+        var second = list.Where(s => !s.IsFreeCondition && HasType(s, SkillType.AttackChanger, SkillType.DefenseChanger)).ToList();
+        if (second.Count > 0)
+            return RandomUtil.DrawRandom(second);
+
+        return RandomUtil.DrawRandom(list);
+    }
+
+    bool HasType(Skill skill, params SkillType[] types)
+    {
+        foreach (var data in skill.SkillDatas)
+        {
+            if (types.Contains(data.TraitType))
+                return true;
+        }
+        return false;
+    }
 }
