acc26bf80529b26d070ecfaf714036dd1837dfb9
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-12-16 18:33:18 +0900

    이벤트 순서 꼬이는 버스 수정

--- Assets/1_Script/Unity/Main/AI_Main.cs
+++ Assets/1_Script/Unity/Main/AI_Main.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class AI_Main : MonoBehaviour, IPhaseEntry
@@ -8,8 +9,14 @@ public class AI_Main : MonoBehaviour, IPhaseEntry
     [SerializeField] SelectorsCreatetorSO selectorsCreatetor;
 
     AI_BanPickAgent banPickAgent;
-    public void EnterBan() => banPickAgent.Ban(Team);
+    public void EnterBan() => StartCoroutine(CoBan());
     public void EnterPick() => banPickAgent.Pick(Team);
+
+    IEnumerator CoBan()
+    {
+        yield return new WaitForSeconds(0.5f);
+        banPickAgent.Ban(Team);
+    }
 
     public void Init(Team team, GameBanPickStorage storage, SlotStorage<Skill> skillSlots, SkillUseController skillUseController)
     {
