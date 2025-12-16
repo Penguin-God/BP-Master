8c018d55afbe7d5169701db5aaad9b8172c9bce8
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-12-16 20:09:02 +0900

    필요없는 코드 제거

--- Assets/1_Script/Application/Mastery/TeamMasteryApplier.cs
+++ Assets/1_Script/Application/Mastery/TeamMasteryApplier.cs
@@ -1,20 +1,6 @@
-using System.Collections.Generic;
-using System.Linq;
 
 public class TeamMasteryApplier
 {
-    public void ApplyMastery(int[] ids, ChampionStatus[] statuses, IEnumerable<ChampionMastery> masteries)
-    {
-        for (int i = 0; i < ids.Length; i++)
-        {
-            if (masteries.Any(x => x.ChampionId == ids[i]))
-            {
-                var mastery = masteries.First(x => x.ChampionId == ids[i]);
-                ApplyStatChange(statuses[i], mastery.Level);
-            }
-        }
-    }
-
     public void ApplyStatChange(ChampionStatus status, int masteryLevel)
     {
         var oldStat = status.Stat;
