812db1d3e732b290c9a6c0a4959e51a9dbca33ad
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-12-17 20:21:22 +0900

    스킬 텍스트 so 생성

---
+++ Assets/1_Script/Unity/SO/SkillTextSO.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class SkillActionTextField
+{
+    public SkillType Type;
+    public string Text;
+}
+
+[CreateAssetMenu(fileName = "SkillTextSO", menuName = "BP Master/SkillTextSO")]
+public class SkillTextSO : ScriptableObject
+{
+    [SerializeField] SkillActionTextField[] skillActionTextFields;
+
+    public SkillTextConverter CreateSkillConverter() => new SkillTextConverter(skillActionTextFields.ToDictionary(x => x.Type, x => x.Text));
+}
