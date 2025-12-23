1b5a3334e8ebfa33d54ab30e1c0aab700afb01cc
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-12-24 00:38:06 +0900

    툴팁 폴더 만들고 트리거 추상화

--- Assets/1_Script/Unity/UI/Tooltip/TooltipTrigger.cs
+++ Assets/1_Script/Unity/UI/Tooltip/TooltipTrigger.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public abstract class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Tooltip Configuration")]
     [SerializeField] TooltipView tooltip;
     [SerializeField] Vector2 tooltipSize = new Vector2(300, 100); // 툴팁 크기
     [SerializeField] Vector3 positionOffset = new Vector3(50, 50, 0); // 틀팁 위치
 
+    protected abstract string BuildText();
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         Vector2 targetPos = transform.position + positionOffset;
-        tooltip.Show("AAAAAAAAAAAAAAAAAA", tooltipSize, targetPos);
+        tooltip.Show(BuildText(), tooltipSize, targetPos);
     }
 
     public void OnPointerExit(PointerEventData eventData)
--- Assets/1_Script/Unity/UI/Tooltip/TooltipView.cs
+++ Assets/1_Script/Unity/UI/Tooltip/TooltipView.cs
@@ -25,4 +25,6 @@ public class TooltipView : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+
 }
