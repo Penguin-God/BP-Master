6d1caf7dd2588507a113caaa66a8435e85a11903
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-01-04 22:26:52 +0900

    프리뷰 안쓰는 점수차 계산 매소드 생성

--- Assets/1_Script/UseCase/AI/ChampEvalueatros/SkillApplyDeltaCalculator.cs
+++ Assets/1_Script/UseCase/AI/ChampEvalueatros/SkillApplyDeltaCalculator.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 
 public record TeamStatChangeInfo(int Att, int Def, int Speed);
@@ -15,12 +14,24 @@ public class SkillApplyDeltaCalculator
         this.originalSlots = originalSlots;
     }
 
+    public SkillApplyDeltaCalculator()
+    {
+    }
+
     public GameStatChangeInfo CalculateApplySkillStat(Champion champion)
     {
         SlotStorage<ChampionStatus> skillApplySlots = previewer.PreviewSkill(champion);
 
         TeamStatChangeInfo blueDelta = CalculateTeamDelta(skillApplySlots, Team.Blue);
         TeamStatChangeInfo redDelta = CalculateTeamDelta(skillApplySlots, Team.Red);
+
+        return new GameStatChangeInfo(blueDelta, redDelta);
+    }
+
+    public GameStatChangeInfo CalculateStatDelta(SlotStorage<ChampionStatus> origin, SlotStorage<ChampionStatus> after)
+    {
+        TeamStatChangeInfo blueDelta = CalculateTeamDelta(origin, after, Team.Blue);
+        TeamStatChangeInfo redDelta = CalculateTeamDelta(origin, after, Team.Red);
 
         return new GameStatChangeInfo(blueDelta, redDelta);
     }
@@ -36,6 +47,27 @@ public class SkillApplyDeltaCalculator
         foreach (var slotData in teamSlots)
         {
             ChampionStatus beforeStatus = originalSlots.GetSlot(slotData);
+            ChampionStatus afterStatus = after.GetSlot(slotData);
+
+            totalAtt += afterStatus.Stat.Attack - beforeStatus.Stat.Attack;
+            totalDef += afterStatus.Stat.Defense - beforeStatus.Stat.Defense;
+            totalSpd += afterStatus.Stat.Speed - beforeStatus.Stat.Speed;
+        }
+
+        return new TeamStatChangeInfo(totalAtt, totalDef, totalSpd);
+    }
+
+    TeamStatChangeInfo CalculateTeamDelta(SlotStorage<ChampionStatus> origin, SlotStorage<ChampionStatus> after, Team team)
+    {
+        int totalAtt = 0;
+        int totalDef = 0;
+        int totalSpd = 0;
+
+        var teamSlots = origin.GetAllSlotDatas().Where(s => s.Team == team);
+
+        foreach (var slotData in teamSlots)
+        {
+            ChampionStatus beforeStatus = origin.GetSlot(slotData);
             ChampionStatus afterStatus = after.GetSlot(slotData);
 
             totalAtt += afterStatus.Stat.Attack - beforeStatus.Stat.Attack;
--- Assets/2_Tests/Domain/AI/AI_DicisionTests/SkillPreviewTests.cs
+++ Assets/2_Tests/Domain/AI/AI_DicisionTests/SkillPreviewTests.cs
@@ -32,4 +32,21 @@ public class SkillPreviewTests
         result = sut.CalculateApplySkillStat(champion);
         Assert.AreEqual(-200, result.Red.Att);
     }
+
+    [Test]
+    public void 스탯_슬롯_전후_점수차_계산()
+    {
+        var originSlots = CreateTwoSlotStatus(att: 1000, def: 100, speed:5);
+        var afterSlots = CreateTwoSlotStatus(att: 800);
+        var sut = new SkillApplyDeltaCalculator();
+
+        var result = sut.CalculateStatDelta(originSlots, afterSlots);
+        Assert.AreEqual(-400, result.Red.Att);
+        Assert.AreEqual(-200, result.Red.Def);
+        Assert.AreEqual(-10, result.Red.Speed);
+
+        Assert.AreEqual(-400, result.Blue.Att);
+        Assert.AreEqual(-200, result.Blue.Def);
+        Assert.AreEqual(-10, result.Blue.Speed);
+    }
 }
