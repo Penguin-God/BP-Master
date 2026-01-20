fc8b7650d27b1a2302d1b844f44748320cc8f40a
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-01-20 16:19:43 +0900

    유일 오브젝트가 승패 관리하는 기능 추가

---
+++ Assets/1_Script/4_Infrastructure/Game/MatchManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MatchManager : MonoBehaviour
+{
+    [SerializeField] int targetWin;
+    MatchRecord _record;
+    public MatchRecord Record => _record;
+
+    void Awake() // 유일객체 구현
+    {
+        var managers = FindObjectsByType<MatchManager>(FindObjectsSortMode.None);
+        if (managers.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        _record = new MatchRecord(targetWin);
+    }
+
+    public void AddWin(Team winner) => _record.AddWin(winner);
+
+    [ContextMenu("Red ")] void 주작() => HandleRoundEnd(Team.Red);
+
+    public void HandleRoundEnd(Team winner)
+    {
+        _record.AddWin(winner);
+        if (_record.IsMatchFinished)
+        {
+            Debug.Log($"최종 승리 팀: {_record.MatchWinner}");
+            return;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
