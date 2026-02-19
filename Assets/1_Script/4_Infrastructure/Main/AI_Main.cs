7f7da1f72697a8adc0f377cbadda61497f6233a3
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-02-19 14:32:55 +0900

    하는일 없는 class 통합 후 제거

--- Assets/1_Script/4_Infrastructure/Main/AI_Main.cs
+++ Assets/1_Script/4_Infrastructure/Main/AI_Main.cs
@@ -7,14 +7,10 @@ public class AI_Main : MonoBehaviour, IPhaseEntry
     [SerializeField] AI_SelectorFactory selectorsCreatetor;
 
     AI_BanPickAgent banPickAgent;
+    AI_SkillExecutionUseCase skillUseCase;
+
     public void EnterBan() => StartCoroutine(CoBan());
     public void EnterPick() => banPickAgent.Pick(Team);
-
-    IEnumerator CoBan()
-    {
-        yield return new WaitForSeconds(0.5f);
-        banPickAgent.Ban(Team);
-    }
 
     public void Init(Team team, BanPickStorage storage, SlotStorage<Skill> skillSlots, SkillUsecase skillUseController, SlotStorage<ChampionStatus> statusSlots, ChampionCatalog championCatalog, MasteryRegistry masteryRegistry, BanPickHandler banPickHandler)
     {
@@ -22,13 +18,25 @@ public class AI_Main : MonoBehaviour, IPhaseEntry
 
         selectorsCreatetor.Init(Team, championCatalog, masteryRegistry.GetTeamMasteryManager(Team), statusSlots);
         banPickAgent = new AI_BanPickAgent(Team, storage, selectorsCreatetor.CreateBanSelector(), selectorsCreatetor.CreatePickSelector(), banPickHandler);
-        storage.OnPick += OnPick;
-        GetComponent<AI_MonoBehaviourAgent>().Init(new AI_SkillExecutionUseCase(skillSlots, skillUseController, new RandomSkillTargetSelector()));
+        banPickHandler.BanPickEventDispatcher.OnPick += OnPick;
+        skillUseCase = new AI_SkillExecutionUseCase(skillSlots, skillUseController, new RandomSkillTargetSelector());
+    }
+
+    IEnumerator CoBan()
+    {
+        yield return new WaitForSeconds(0.5f);
+        banPickAgent.Ban(Team);
     }
 
     void OnPick(SlotData slotData, int id)
     {
         if (slotData.Team != Team) return;
-        GetComponent<AI_MonoBehaviourAgent>().UseSkill(slotData);
+        StartCoroutine(Co_UseSkill(slotData));
+    }
+
+    IEnumerator Co_UseSkill(SlotData slot)
+    {
+        yield return new WaitForSeconds(1.5f);
+        skillUseCase.UseSkill(slot);
     }
 }
