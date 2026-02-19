4da7e1c625beb1ce214dafb55762c40f7b3a9e9d
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-02-19 20:47:18 +0900

    솔트 기능 추가

--- Assets/1_Script/4_Infrastructure/UI/Match/View/ChampionButtonView.cs
+++ Assets/1_Script/4_Infrastructure/UI/Match/View/ChampionButtonView.cs
@@ -6,16 +6,48 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
+public enum ChampionSortType
+{
+    Default,
+    Attack,
+    Defense,
+    Speed
+}
+
 public class ChampionButtonView : MonoBehaviour
 {
     [SerializeField] ChampionRepository championManager;
-    [SerializeField] GameObject championBtn;
+    [SerializeField] GameObject championBtnPrefab;
     [SerializeField] Transform content;
+
+    [Header("Tab Buttons")]
+    [SerializeField] Button defaultTabBtn;
+    [SerializeField] Button attackTabBtn;
+    [SerializeField] Button defenseTabBtn;
+    [SerializeField] Button speedTabBtn;
 
     IEnumerable<Button> buttons;
     public IEnumerable<Button> Buttons => buttons;
 
-    public void CreateButtons() => buttons = new ChampionButtonCreator().DrawChampionButtons(content, championManager.AllChampion, championBtn);
+    UnityAction<ChampionIdentify> clickEvent;
+
+    void Start()
+    {
+        var champions = championManager.AllChampion.Select(x => x.CreateChampion());
+        defaultTabBtn.onClick.AddListener(() => GetTabIds(champions, ChampionSortType.Default));
+        attackTabBtn.onClick.AddListener(() => GetTabIds(champions, ChampionSortType.Attack));
+        defenseTabBtn.onClick.AddListener(() => GetTabIds(champions, ChampionSortType.Default));
+        speedTabBtn.onClick.AddListener(() => GetTabIds(champions, ChampionSortType.Speed));
+    }
+
+    public void CreateButtons() => CreateButtons(championManager.AllId);
+
+    void CreateButtons(IEnumerable<int> Ids)
+    {
+        foreach (Transform child in content) Destroy(child.gameObject);
+        buttons = new ChampionButtonCreator().DrawChampionButtons(content, Ids.Select(x => championManager.GetChampionData(x)), championBtnPrefab);
+        AppluEvnet(clickEvent);
+    }
 
     public Button GetButton(int id) => buttons.First(x => x.GetComponent<ChampionIdentify>().Id == id);
     public void InActiveButton(int id)
@@ -23,7 +55,14 @@ public class ChampionButtonView : MonoBehaviour
         ButtonUtil.InActiveButton(GetButton(id));
         GetButton(id).GetComponentInChildren<TextMeshProUGUI>().color = new Color32(60, 60, 60, 255);
     }
+
     public void AddEvent(UnityAction<ChampionIdentify> action)
+    {
+        clickEvent += action;
+        AppluEvnet(action);
+    }
+
+    void AppluEvnet(UnityAction<ChampionIdentify> action)
     {
         foreach (var btn in buttons)
             btn.onClick.AddListener(() => action(btn.GetComponent<ChampionIdentify>()));
@@ -34,6 +73,16 @@ public class ChampionButtonView : MonoBehaviour
         foreach (var btn in buttons.Where(x => selectableIds.Contains(x.GetComponent<ChampionIdentify>().Id) == false))
             InActiveButton(btn.GetComponent<ChampionIdentify>().Id);
     }
+
+    readonly ChampionStatSorter championStatSorter = new ChampionStatSorter();
+    IEnumerable<int> GetTabIds(IEnumerable<Champion> allChampions, ChampionSortType sortType) => sortType switch
+    {
+        ChampionSortType.Default => championManager.AllId,
+        ChampionSortType.Attack => championStatSorter.SortByStat(allChampions, StatType.Attack).Select(x => x.Id),
+        ChampionSortType.Defense => championStatSorter.SortByStat(allChampions, StatType.Defense).Select(x => x.Id),
+        ChampionSortType.Speed => championStatSorter.SortByStat(allChampions, StatType.Speed).Select(x => x.Id),
+        _ => throw new System.NotImplementedException(),
+    };
 }
 
 public class ChampionButtonCreator
