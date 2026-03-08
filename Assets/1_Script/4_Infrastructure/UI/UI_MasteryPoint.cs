029802ad646099f20c8437ad45219f4b0a7b68d8
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-03-08 14:14:31 +0900

    ui 틀 제작

--- Assets/1_Script/4_Infrastructure/UI/UI_MasteryPoint.cs
+++ Assets/1_Script/4_Infrastructure/UI/UI_MasteryPoint.cs
@@ -1,6 +1,17 @@
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 
 public class UI_MasteryPoint : MonoBehaviour
 {
-    
+    [AssetsOnly] [SerializeField] GameObject btnPerfab;
+    [SerializeField] Transform parent;
+    [SerializeField] TextMeshProUGUI _pointText;
+
+    void Start()
+    {
+        foreach (Transform child in parent) Destroy(child.gameObject);
+
+        new ChampionButtonCreator().DrawChampionButtons(parent, ChampionDataLoder.AllChampions, btnPerfab);
+    }
 }
