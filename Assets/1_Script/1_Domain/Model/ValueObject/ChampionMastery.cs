60f4d19dd4dbbbda1cae8a33cac2726c193cb582
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-03-12 19:33:53 +0900

    stat의 공을 lv 대신 써서 제거

--- Assets/1_Script/1_Domain/Model/ValueObject/ChampionMastery.cs
+++ Assets/1_Script/1_Domain/Model/ValueObject/ChampionMastery.cs
@@ -1,19 +1,16 @@
 public readonly struct ChampionMastery
 {
     public readonly int ChampionId;
-    public readonly int Level;
     public readonly ChampionStatData MasteryStat;
     public ChampionMastery(int championId, int level)
     {
         ChampionId = championId;
-        Level = level;
         MasteryStat = new ChampionStatData(level, level, 0);
     }
 
     public ChampionMastery(int championId, ChampionStatData masteryStat)
     {
         ChampionId = championId;
-        Level = 0;
         MasteryStat = masteryStat;
     }
 }
--- Assets/1_Script/1_Domain/Service/Mastery/MasteryCollection.cs
+++ Assets/1_Script/1_Domain/Service/Mastery/MasteryCollection.cs
@@ -8,7 +8,7 @@ public class MasteryCollection
 
     public MasteryCollection(IEnumerable<ChampionMastery> masteries) => masteryMap = masteries.ToDictionary(m => m.ChampionId, m => m);
 
-    public int GetMasteryLevel(int championId) => HasMastery(championId) ? masteryMap[championId].Level : 0;
+    public int GetMasteryLevel(int championId) => HasMastery(championId) ? masteryMap[championId].MasteryStat.Attack : 0;
     public ChampionStatData GetMasteryStat(int championId) => HasMastery(championId) ? masteryMap[championId].MasteryStat : new ChampionStatData(0, 0, 0);
     public bool HasMastery(int championId) => masteryMap.ContainsKey(championId);
 }
