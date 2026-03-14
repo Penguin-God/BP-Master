ff107d0eb9af82307f49e64e06e86089b65a67aa
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-03-14 17:43:40 +0900

    죽은 코드 제거

--- Assets/1_Script/1_Domain/Model/PlayerData.cs
+++ Assets/1_Script/1_Domain/Model/PlayerData.cs
@@ -1,18 +1,9 @@
-using log4net.Appender;
-using System.Collections.Generic;
 
 public class PlayerData
 {
-    public readonly int Id = 0;
+    public readonly int Id ;
     public readonly string Name;
-    public readonly MasteryCollection Mastery;
-    public MasteryBoardCollection MasteryBoardCollection;
-
-    public PlayerData(string name, MasteryCollection mastery)
-    {
-        Name = name;
-        Mastery = mastery;
-    }
+    public readonly MasteryBoardCollection MasteryBoardCollection;
 
     public PlayerData(int id, string name, MasteryBoardCollection masteryBoardCollection)
     {
@@ -20,13 +11,4 @@ public class PlayerData
         Name = name;
         MasteryBoardCollection = masteryBoardCollection;
     }
-}
-
-public class ParticipantRepository
-{
-    readonly Dictionary<Participant, PlayerData> _datas = new();
-
-    public void Save(Participant p, PlayerData data) => _datas[p] = data;
-
-    public PlayerData Get(Participant p) => _datas[p];
 }
