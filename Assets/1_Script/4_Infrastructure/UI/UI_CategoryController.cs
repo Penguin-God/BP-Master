746ec8359267b5882726dbd05d880fe755aea7de
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-04-02 16:05:16 +0900

    카테고리 기능 추가

---
+++ Assets/1_Script/4_Infrastructure/UI/UI_CategoryController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CategoryController : MonoBehaviour
+{
+    [Header("버튼")]
+    [SerializeField] Button leagueBtn;
+    [SerializeField] Button masteryBtn;
+
+    [Header("패널")]
+    [SerializeField] GameObject schedulePanel;
+    [SerializeField] GameObject masteryPanel;
+
+    void Awake()
+    {
+        leagueBtn.onClick.AddListener(ShowSchedule);
+        masteryBtn.onClick.AddListener(ShowMastery);
+    }
+
+    void Start()
+    {
+        // 로비 진입 시 기본적으로 리그(스케줄) 화면을 보여줍니다.
+        ShowSchedule();
+    }
+
+    void ShowSchedule()
+    {
+        schedulePanel.SetActive(true);
+        masteryPanel.SetActive(false);
+    }
+
+    void ShowMastery()
+    {
+        schedulePanel.SetActive(false);
+        masteryPanel.SetActive(true);
+    }
+}
