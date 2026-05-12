9cb4dc1fde321b9b2da983201623db75c3d48412
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-05-12 23:27:52 +0900

    SO 데이터 추가

--- Assets/1_Script/4_Infrastructure/SO/Data/TutorialTriggerSO.cs
+++ Assets/1_Script/4_Infrastructure/SO/Data/TutorialTriggerSO.cs
@@ -5,23 +5,20 @@ using System.Linq;
 public class TutorialEntry
 {
     public TutorialType Type;
-    [TextArea(3, 5)] public string[] Dialogues;
+    public string[] Dialogues;
 }
 
-[CreateAssetMenu(fileName = "TutorialTriggerSO", menuName = "BP Master/TutorialTriggerSO")]
+[CreateAssetMenu(fileName = "TutorialTriggerSO", menuName = "Data/TutorialTriggerSO")]
 public class TutorialTriggerSO : ScriptableObject, ITutorialViewer
 {
     [SerializeField] TutorialEntry[] entries;
 
     UI_Tutorial _uiTutorial;
 
-    // 구체적인 UI 클래스를 직접 주입받습니다.
     public void Inject(UI_Tutorial uiTutorial) => _uiTutorial = uiTutorial;
 
-    public TutorialTriggerUseCase CreateUseCase()
-        => new TutorialTriggerUseCase(new PlayerPrefsTutorialStorage(), this);
+    public void StartTutorial(TutorialType type) => new TutorialTriggerUseCase(new PlayerPrefsTutorialStorage(), this).TriggerIfFirstTime(type);
 
-    // ITutorialViewer 구현부
     public void Show(TutorialType type)
     {
         var dialogues = entries.FirstOrDefault(x => x.Type == type)?.Dialogues;
