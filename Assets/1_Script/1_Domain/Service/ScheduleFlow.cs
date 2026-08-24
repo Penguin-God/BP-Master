75e39c2603c87ea52960be2db782fb6fcc3ee6d4
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-08-24 19:31:58 +0900

    스케줄 관련 스크립트 제거

--- Assets/1_Script/1_Domain/Service/ScheduleFlow.cs
+++ Assets/1_Script/1_Domain/Service/ScheduleFlow.cs
@@ -6,27 +6,3 @@ public record MatchData(int Id1, int Id2)
     public IEnumerable<int> All_Ids => new int[] { Id1, Id2 };
     public int GetOpponentId(int id) => All_Ids.Except(new int[] { id }).First();
 };
-
-
-public class ScheduleFlow
-{
-    readonly MatchData[] _matches;
-    public IReadOnlyList<MatchData> Matches => _matches;
-    public int CurrentIndex { get; private set; }
-
-    public ScheduleFlow(IEnumerable<MatchData> matchDatas, int startIndex = 0)
-    {
-        _matches = matchDatas.ToArray();
-        CurrentIndex = startIndex;
-    }
-
-    public MatchData CurrentMatch => _matches[CurrentIndex];
-    public bool IsFinished => CurrentIndex >= _matches.Length;
-
-    public MatchData Advance()
-    {
-        var match = _matches[CurrentIndex];
-        CurrentIndex++;
-        return match;
-    }
-}
