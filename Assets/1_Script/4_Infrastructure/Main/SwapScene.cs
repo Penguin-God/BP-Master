1e86b151df8d40c81f9c7f10b5893f66bc1b70f6
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-08-25 16:40:21 +0900

    스왑에서 배틀 씬으로 넘어가는 버튼과 조건 구현

--- Assets/1_Script/4_Infrastructure/Main/SwapScene.cs
+++ Assets/1_Script/4_Infrastructure/Main/SwapScene.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using Match;
+using UnityEngine.UI;
+using System.Linq;
 
 public class SwapScene : MonoBehaviour
 {
+    [SerializeField] Button nextBattleBtn;
     DeckBuildStore store;
     void Awake()
     {
         store = new DeckBuildStore(MatchContext.CurrentDeck);
         store.OnStateChanged += Change;
         FindAnyObjectByType<UI_DeckBuilder>().Init(store, id => MatchContext.Storage.CanSelected(id) ? Color.white : Color.gray);
+        nextBattleBtn.onClick.AddListener(() => SceneLoadHelper.LoadScene(SceneType.Battle));
+        nextBattleBtn.interactable = CheckDeckPlayable(MatchContext.CurrentDeck);
     }
 
     void OnDestroy()
@@ -17,10 +22,16 @@ public class SwapScene : MonoBehaviour
             store.OnStateChanged -= Change;
     }
 
-    void Change(DeckBuildState state) => MatchContext.CurrentDeck = store.State;
-
-    void Update()
+    void Change(DeckBuildState state)
     {
-        if (Input.GetKeyDown(KeyCode.Space)) SceneLoadHelper.LoadScene(SceneType.Battle);
+        MatchContext.CurrentDeck = store.State;
+        nextBattleBtn.interactable = CheckDeckPlayable(state);
+    }
+
+    bool CheckDeckPlayable(DeckBuildState state)
+    {
+        bool isDeckFull = state.SelectedCards.Count == state.CardCount;
+        bool isAllCardsValid = state.SelectedCards.All(id => MatchContext.Storage.CanSelected(id));
+        return isDeckFull && isAllCardsValid;
     }
 }
