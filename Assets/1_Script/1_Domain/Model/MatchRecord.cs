ea0c9720de57069e5e999a3442eff3f2a54e7853
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-08-25 19:34:17 +0900

    안쓰는 코드 제거

--- Assets/1_Script/1_Domain/Model/MatchRecord.cs
+++ Assets/1_Script/1_Domain/Model/MatchRecord.cs
@@ -1,25 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public enum Participant { Player, AI }
-
-public class MatchRecord
-{
-    public int PlayerWinCount { get; private set; }
-    public int AiWinCount { get; private set; }
-    public int TargetWins { get; }
-
-    public MatchRecord(int targetWins = 2) => TargetWins = targetWins;
-
-    public void AddWin(Participant winner)
-    {
-        if (winner == Participant.Player) PlayerWinCount++;
-        else if (winner == Participant.AI) AiWinCount++;
-    }
-
-    public bool IsMatchFinished => PlayerWinCount >= TargetWins || AiWinCount >= TargetWins;
-    public Participant MatchWinner => PlayerWinCount >= TargetWins ? Participant.Player : Participant.AI;
-}
 
 public class MatchWinCounter
 {
